5257aee8e2fec721b17715549ffe4210f9904729
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2023-06-23 23:13:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,7 +1,6 @@
 from os import environ
 from logging import basicConfig, INFO, StreamHandler, getLogger, WARNING, Logger
 from logging.handlers import RotatingFileHandler
-from script import StartTxT, HelpTxT, AboutTxT
 
 basicConfig( level=INFO, format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s", datefmt='%d-%b-%y %H:%M:%S', handlers=[ RotatingFileHandler("filtersbot.txt", maxBytes=50000000, backupCount=10), StreamHandler() ] )
 

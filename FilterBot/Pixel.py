1533e28c3d04ce413df29dc4bf801d3993af60e7
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2023-06-23 18:17:06 +0530

    Update Pixel.py

--- FilterBot/Pixel.py
+++ FilterBot/Pixel.py
@@ -1,8 +1,8 @@
 import os
 import pixeldrain
-from pyrogram import Client as Bot, filters
+from pyrogram import Client as Bot, filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
+from Help.Grapic import progress_for_pyrogram, convert
 
 @Bot.on_message(filters.private & filters.command("start"))
 async def start(bot, update):
@@ -15,7 +15,9 @@ async def start(bot, update):
 
 @Bot.on_message(filters.private & filters.media)
 async def media_filter(bot, update):
-    
+
+    ms = await update.reply_text(text="𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳...", quote=True, parse_mode=enums.ParseMode.DEFAULT)
+    c_time = time.time()
     logs = []
     message = await update.reply_text(
         text="`Processing...`",
@@ -26,9 +28,10 @@ async def media_filter(bot, update):
     try:
         # download
         try:
-            await message.edit_text(
-                text="`Downloading...`",
-                disable_web_page_preview=True
+            await ms.edit_text(
+                progress=progress_for_pyrogram,
+                progress_args=( "Downloading.....\n\nEngine : User",  ms, c_time   ),
+                parse_mode=enums.ParseMode.DEFAULT
             )
         except:
             pass
@@ -36,10 +39,12 @@ async def media_filter(bot, update):
         logs.append("Download Successfully")
         
         # upload
+        c_time = time.time()
         try:
-            await message.edit_text(
-                text="`Uploading...`",
-                disable_web_page_preview=True
+            await ms.edit_text(
+                progress=progress_for_pyrogram,
+                progress_args=( "uploading.....\n\nEngine : User",  ms, c_time   ),
+                parse_mode=enums.ParseMode.DEFAULT
             )
         except:
             pass
